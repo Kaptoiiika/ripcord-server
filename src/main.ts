f77bcd3ef5f4d0d4039afa97f2852b3d0070c8d0
Nikita Ozhegov <nikita.ozhegov8@mail.ru>
2023-04-24 08:54:40 +0400

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors();
-  await app.listen(1337)
+  await app.listen(1338)
 }
 bootstrap()
